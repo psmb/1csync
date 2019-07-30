843d3a88337cbd2bd50fd7472792c3004aba115c
Dmitri Pisarev <dimaip@gmail.com>
2019-07-30 16:07:09 +0300

    Sync catgories

--- 1csync.go
+++ 1csync.go
@@ -18,13 +18,71 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/joho/godotenv"
+	"pkg.re/essentialkaos/translit.v2"
 )
 
-var syliusToken string
+var _syliusToken string
+
+func urlify(url string) string {
+	return strings.ToLower(strings.Replace(translit.EncodeToICAO(url), " ", "-", -1))
+}
+
+func syncManufacturers() {
+	url := "/1cbooks/odata/standard.odata/Catalog_%D0%9F%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D0%B8/?$format=json"
+
+	manufacterersR := odinCRequest("GET", url, nil)
+	for _, manufacturerRaw := range manufacterersR["value"].([]interface{}) {
+		manufacterer := manufacturerRaw.(map[string]interface{})
+		code := manufacterer["Ref_Key"].(string)
+		name := manufacterer["Description"].(string)
+		body, _ := json.Marshal(map[string]interface{}{
+			"code":   code,
+			"parent": "publishers",
+			"translations": map[string]interface{}{
+				"ru_RU": map[string]string{
+					"name": name,
+					"slug": "category/publishers/" + urlify(name),
+				},
+			},
+		})
+		syliusRequest("PUT", "/api/v1/taxons/"+code, bytes.NewReader(body), "application/json")
+	}
+}
+
+var validCategories map[string]bool
+
+func syncCategories() {
+	url := "/1cbooks/odata/standard.odata/Catalog_%D0%97%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D1%8F%D0%A1%D0%B2%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%9E%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2%D0%98%D0%B5%D1%80%D0%B0%D1%80%D1%85%D0%B8%D1%8F/?$format=json"
+
+	catgoriesR := odinCRequest("GET", url, nil)
+	validCategories = make(map[string]bool)
+	for _, categoriesRaw := range catgoriesR["value"].([]interface{}) {
+		category := categoriesRaw.(map[string]interface{})
+		code := category["Ref_Key"].(string)
+		parentKey := category["Parent_Key"].(string)
+		name := category["Description"].(string)
+		if parentKey == "79da890c-ac54-11e9-a4b0-08606ed6b998" {
+			body, _ := json.Marshal(map[string]interface{}{
+				"code":   code,
+				"parent": "books",
+				"translations": map[string]interface{}{
+					"ru_RU": map[string]string{
+						"name": name,
+						"slug": "category/books/" + urlify(name),
+					},
+				},
+			})
+			syliusRequest("PUT", "/api/v1/taxons/"+code, bytes.NewReader(body), "application/json")
+			validCategories[code] = true
+		}
+	}
+}
 
 func fetchSyliusToken() {
-	link := "/api/oauth/v2/token"
+	syliusHost, _ := os.LookupEnv("SYLIUS_HOST")
+	link := syliusHost + "/api/oauth/v2/token"
 
 	syliusClientID, _ := os.LookupEnv("SYLIUS_CLIENT_ID")
 	syliusClientSecret, _ := os.LookupEnv("SYLIUS_CLIENT_SECRET")
@@ -54,7 +112,7 @@ func fetchSyliusToken() {
 		panic(errJSON)
 	}
 
-	syliusToken = decodedBody["access_token"].(string)
+	_syliusToken = decodedBody["access_token"].(string)
 }
 
 func initApp() {
@@ -64,13 +122,15 @@ func initApp() {
 	}
 
 	fetchSyliusToken()
+	syncManufacturers()
+	syncCategories()
 }
 
 func syliusRequest(requestType string, url string, body io.Reader, contentType string) map[string]interface{} {
 	syliusHost, _ := os.LookupEnv("SYLIUS_HOST")
 	req, errRequest := http.NewRequest(requestType, syliusHost+url, body)
 	req.Header.Set("Content-Type", contentType)
-	req.Header.Set("Authorization", "Bearer "+syliusToken)
+	req.Header.Set("Authorization", "Bearer "+_syliusToken)
 	client := &http.Client{}
 	if errRequest != nil {
 		panic(errRequest)
@@ -86,6 +146,12 @@ func syliusRequest(requestType string, url string, body io.Reader, contentType s
 	}
 	var decodedBody map[string]interface{}
 	errJSON := json.Unmarshal(respBody, &decodedBody)
+	if resp.StatusCode >= 500 && requestType != "DELETE" {
+		color.Magenta("PANIC!")
+		fmt.Println(req)
+		fmt.Println(decodedBody)
+		panic(decodedBody)
+	}
 	if errJSON != nil {
 		return map[string]interface{}{
 			"body": respBody,
@@ -123,8 +189,13 @@ func odinCRequest(requestType string, url string, body io.Reader) map[string]int
 }
 
 func importProduct(sourceProduct map[string]interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
 	sourceProductID := sourceProduct["Ref_Key"].(string)
-	slug := sourceProduct["Артикул"].(string)
+	slug := strings.Replace(sourceProduct["Артикул"].(string), " ", "-", -1)
 
 	fmt.Println("=== Importing product: " + slug + "===")
 
@@ -132,12 +203,31 @@ func importProduct(sourceProduct map[string]interface{}) {
 	// @see: https://github.com/Sylius/Sylius/issues/10532
 	syliusRequest("DELETE", "/api/v1/products/"+slug, nil, "application/json")
 
+	var productTaxons []string
+	manufacturerKey := sourceProduct["Производитель_Key"].(string)
+	if len(manufacturerKey) > 0 && manufacturerKey != "00000000-0000-0000-0000-000000000000" {
+		productTaxons = append(productTaxons, sourceProduct["Производитель_Key"].(string))
+	}
+	dops := sourceProduct["ДополнительныеРеквизиты"].([]interface{})
+
+	for _, dopRaw := range dops {
+		dop := dopRaw.(map[string]interface{})
+		if dop["Свойство_Key"].(string) == "52f8b02d-552e-11e9-907f-14dae924f847" && validCategories[dop["Значение"].(string)] {
+			productTaxons = append(productTaxons, dop["Значение"].(string))
+		}
+	}
+
 	productData := map[string]interface{}{
-		"code":                      slug,
-		"enabled":                   true,
-		"channels[0]":               "default",
-		"translations[ru_RU][name]": sourceProduct["НаименованиеПолное"].(string),
-		"translations[ru_RU][slug]": slug,
+		"code":                             slug,
+		"enabled":                          true,
+		"channels[0]":                      "default",
+		"translations[ru_RU][name]":        sourceProduct["НаименованиеПолное"].(string),
+		"translations[ru_RU][description]": sourceProduct["Описание"].(string),
+		"translations[ru_RU][slug]":        slug,
+	}
+	productTaxonsString := strings.Join(productTaxons, ",")
+	if len(productTaxonsString) > 0 {
+		productData["productTaxons"] = productTaxonsString
 	}
 
 	fmt.Println("Get images")
@@ -175,46 +265,28 @@ func importProduct(sourceProduct map[string]interface{}) {
 		productData["images["+index+"][type]"] = imageType
 	}
 
-	// ДополнительныеРеквизиты
-	// /1cbooks/odata/standard.odata/ChartOfCharacteristicTypes_%D0%94%D0%BE%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5%D0%A0%D0%B5%D0%BA%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D1%8B%D0%98%D0%A1%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F/?$format=json
-	// Parent_Key Группа списка
-	// /1cbooks/odata/standard.odata/Catalog_%D0%A1%D0%B5%D0%B3%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D1%8B/?$format=json
-	// ВидНоменклатуры_Key
-	// /1cbooks/odata/standard.odata/Catalog_%D0%92%D0%B8%D0%B4%D1%8B%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D1%8B/?$format=json
-	// Производитель_Key
-	// /1cbooks/odata/standard.odata/Catalog_%D0%9F%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D0%B8/?$format=json
-
 	body, contentType := makeMultipartBody(productData)
-	resp := syliusRequest("POST", "/api/v1/products/", body, contentType)
-	fmt.Println(resp)
+	result := syliusRequest("POST", "/api/v1/products/", body, contentType)
+
+	if val, ok := result["errors"]; ok {
+		color.Red("ERROR!")
+		fmt.Println(productData)
+		fmt.Println(val)
+	}
 }
 
 func main() {
 	fmt.Println("Syncing 1C and Sylius")
 	initApp()
 
-	// /1cbooks/odata/standard.odata/Catalog_%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D0%B0/?%24format=json&%24filter=%D0%90%D1%80%D1%82%D0%B8%D0%BA%D1%83%D0%BB%20ne%20%27%27%20and%20Description%20eq%20%27%D0%97%D0%B2%D1%83%D0%BA%D0%B8%20%D0%B4%D1%83%D1%88%D0%B8.%20%D0%9D.%D0%9D.%20%D0%9D%D0%B5%D0%BF%D0%BB%D1%8E%D0%B5%D0%B2%D0%B0%27
 	fmt.Println("Get products from 1C")
 	productsRaw := odinCRequest("GET", "/1cbooks/odata/standard.odata/Catalog_%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D0%B0/?$format=json&$filter=%D0%90%D1%80%D1%82%D0%B8%D0%BA%D1%83%D0%BB%20ne%20%27%27", nil)
 	products := productsRaw["value"].([]interface{})
 	for _, productRaw := range products {
-		defer func() {
-			if r := recover(); r != nil {
-				fmt.Println(r.(error))
-			}
-		}()
+
 		sourceProduct := productRaw.(map[string]interface{})
 		importProduct(sourceProduct)
 	}
-
-	//
-	// Ref_Key
-	// /1cbooks/odata/standard.odata/InformationRegister_ДвоичныеДанныеФайлов(Файл='a58b6122-d46f-11e8-9bf5-14dae924f847', Файл_Type='StandardODATA.Catalog_НоменклатураПрисоединенныеФайлы')/?$format=json
-	// ДвоичныеДанныеФайла_Base64Data \r\n
-
-	// /1cbooks/odata/standard.odata/Catalog_%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D0%B0/?$format=json
-	// /1cbooks/odata/standard.odata/Catalog_НоменклатураПрисоединенныеФайлы/?$format=json
-	// /1cbooks/odata/standard.odata/InformationRegister_ДвоичныеДанныеФайлов(Файл='a58b6122-d46f-11e8-9bf5-14dae924f847', Файл_Type='StandardODATA.Catalog_НоменклатураПрисоединенныеФайлы')/?$format=json
 }
 
 func makeMultipartBody(values map[string]interface{}) (body io.Reader, contentType string) {
