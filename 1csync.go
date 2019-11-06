d1d3313309cebc52caae236488629ffed9b28b55
Dmitri Pisarev <dimaip@gmail.com>
2019-11-06 15:57:15 +0300

    Digital goods sync

--- 1csync.go
+++ 1csync.go
@@ -477,7 +477,7 @@ func importProduct(sourceProduct map[string]interface{}) {
 				"shippingRequired": true,
 				"translations": map[string]interface{}{
 					"ru_RU": map[string]string{
-						"name": "бумажный вариант",
+						"name": "бумажная книга",
 					},
 				},
 				"channelPricings": map[string]interface{}{
@@ -538,7 +538,88 @@ func importProduct(sourceProduct map[string]interface{}) {
 			spew.Dump(val)
 		}
 	}
+}
 
+func importVariant(sourceProduct map[string]interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	sourceProductID := sourceProduct["Ref_Key"].(string)
+	variantSlug := sourceProduct["Артикул"].(string)
+	productSlug := strings.Split(variantSlug, "_")[0]
+
+	logVerbose("=== Importing variant: " + variantSlug + "===")
+
+	if priceItem, ok := _prices[sourceProductID]; ok {
+		variantBody, _ := json.Marshal(map[string]interface{}{
+			"code":             variantSlug,
+			"tracked":          false,
+			"shippingRequired": false,
+			"translations": map[string]interface{}{
+				"ru_RU": map[string]string{
+					"name": "цифровая книга",
+				},
+			},
+			"channelPricings": map[string]interface{}{
+				"default": map[string]float64{
+					"price": priceItem.(map[string]interface{})["Цена"].(float64),
+				},
+			},
+		})
+		variantsResult := syliusPutRequest("/api/v1/products/"+productSlug+"/variants/", variantSlug, bytes.NewReader(variantBody), "application/json")
+		if val, ok := variantsResult["errors"]; ok {
+			color.Red("ERROR variants!")
+			fmt.Println(val)
+		}
+	} else {
+		syliusRequest("DELETE", "/api/v1/products/"+productSlug+"/variants/"+variantSlug, nil, "application/json")
+		color.Yellow("Price not available, deleted variant")
+	}
+
+	logVerbose("Get images")
+	imagesRaw := odinCRequest("GET", "/1cbooks/odata/standard.odata/Catalog_%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%9F%D1%80%D0%B8%D1%81%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B/?$format=json&%24filter=%D0%92%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D0%B5%D1%86%D0%A4%D0%B0%D0%B9%D0%BB%D0%B0_Key%20eq%20guid%27"+sourceProductID+"%27", nil)
+	images := imagesRaw["value"].([]interface{})
+	isFirst := true
+	imagesData := map[string]interface{}{}
+	for i, imageRaw := range images {
+		index := strconv.Itoa(i)
+		imageRaw := imageRaw.(map[string]interface{})
+		imageID := imageRaw["Ref_Key"].(string)
+		ext := imageRaw["Расширение"].(string)
+		title := imageRaw["Description"].(string)
+		description := imageRaw["Описание"].(string)
+		isPaid := description == "$"
+		logVerbose("- Get image data")
+		imageDataRaw := odinCRequest("GET", "/1cbooks/odata/standard.odata/InformationRegister_%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D1%8B%D0%B5%D0%94%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%D0%A4%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2(%D0%A4%D0%B0%D0%B9%D0%BB='"+imageID+"',%20%D0%A4%D0%B0%D0%B9%D0%BB_Type='StandardODATA.Catalog_%D0%9D%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%9F%D1%80%D0%B8%D1%81%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B')/?$format=json", nil)
+		base64image := imageDataRaw["ДвоичныеДанныеФайла_Base64Data"].(string)
+		base64imageFixed := strings.ReplaceAll(base64image, "\r\n", "")
+		imageData, _ := base64.StdEncoding.DecodeString(base64imageFixed)
+
+		var imageType string
+		if isPaid {
+			imageType = "paid"
+		} else if ext == "pdf" {
+			imageType = "pdf"
+		} else if isFirst {
+			isFirst = false
+			imageType = "main"
+		} else {
+			imageType = "default"
+		}
+
+		imagesData["images["+index+"][file]"] = imageData
+		imagesData["images["+index+"][type]"] = imageType
+		imagesData["images["+index+"][title]"] = title
+		imagesData["images["+index+"][productVariants]"] = variantSlug
+	}
+	body, contentType := makeMultipartBody(imagesData)
+	imagesResult := syliusRequest("POST", "/api/v1/products/"+productSlug+"?_method=PATCH", body, contentType)
+	if val, ok := imagesResult["errors"]; ok {
+		color.Red("ERROR images!")
+		spew.Dump(val)
+	}
 }
 
 func main() {
@@ -558,9 +639,13 @@ func main() {
 	products := productsRaw["value"].([]interface{})
 	for _, productRaw := range products {
 		sourceProduct := productRaw.(map[string]interface{})
-		importProduct(sourceProduct)
 		slug := sourceProduct["Артикул"].(string)
-		_newProducts = append(_newProducts, slug)
+		if strings.HasSuffix(slug, "_ebook") {
+			importVariant(sourceProduct)
+		} else {
+			importProduct(sourceProduct)
+			_newProducts = append(_newProducts, slug)
+		}
 	}
 
 	for _, slug := range _existingProducts {
